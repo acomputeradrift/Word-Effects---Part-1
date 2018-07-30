0bfd0e4917c32b0dc798a5c274922b9ef017d777
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-30 14:12:38 -0700

    Inital commit, this works

--- Word Effects - Part 1/main.m
+++ Word Effects - Part 1/main.m
@@ -10,8 +10,24 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        char inputChars[255];//declare array
+        
+        while (true){
+        
+        printf("Input a string: ");
+        fgets(inputChars, 255, stdin);// get user input, limit input to max 255 characters
+        
+        //performs some string manipulation
+       
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+
+        
+        //logs the output back to the user
+        
+        
+        NSLog(@"%@", inputString);
+        }
     }
     return 0;
 }
